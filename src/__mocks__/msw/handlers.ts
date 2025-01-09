adc392710865732982bf5c078b1b24ab4a5f8b6d
Fred Shepherd <fmbshepherd@gmail.com>
2025-01-09 10:38:25 +0000

    Setup: empty project

--- src/__mocks__/msw/handlers.ts
+++ src/__mocks__/msw/handlers.ts
@@ -1,17 +1,13 @@
 import { http, HttpResponse } from 'msw'
 
-export const result = {
-  id: 'c7b3d8e0-5e0b-4b0f-8b3a-3b9f4b3d3b3d',
-  firstName: 'John',
-  lastName: 'Maverick'
-} as const
+export const successResponse = {} as const
 
-export type Result = typeof result
+const errorResponse = {} as const
+
+export type Response = typeof successResponse | typeof errorResponse
 
 export const handlers = [
-  // Intercept "GET https://example.com/user" requests...
-  http.get('https://example.com/user', () => {
-    // ...and respond to them using this JSON response.
-    return HttpResponse.json(result)
+  http.get('/api/example', async () => {
+    return HttpResponse.json(successResponse)
   })
 ]
--- src/components/App.test.tsx
+++ src/components/App.test.tsx
@@ -1,29 +1,13 @@
-import { render, screen } from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
+import { render } from '@testing-library/react'
+
 import App from './App'
 import Providers from 'Providers'
-import { result } from '__mocks__/msw/handlers'
 
 describe('<App />', () => {
-  it('should render the App', async () => {
-    const { container } = render(<App />, {
+  it('should return weather result for London', async () => {
+    render(<App />, {
       wrapper: Providers
     })
-    const user = userEvent.setup()
-
-    expect(
-      screen.getByRole('heading', {
-        level: 1
-      })
-    ).toBeInTheDocument()
-
-    user.click(container)
-
-    expect(screen.getByText(/Hello World/i)).toBeInTheDocument()
-
-    expect(screen.getByTestId('loader').innerText).toBe('Loading')
-
-    await screen.findByTestId('data')
-    expect(screen.getByTestId('data').innerText).toBe(result.firstName)
+    // const user = userEvent.setup()
   })
 })
